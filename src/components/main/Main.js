0d1ff62999231e586e3a61dbc5bf108c53bee64d
Tryd0g0lik <work80@mail.ru>
2023-12-07 16:31:23 +0700

    [03]: It's a first commit. This maket an open new bank  was  completed

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,18 +2,29 @@ import React from "react";
 import StatisticsFC from './Statistics.js'
 import AnalyticsFC from "./analys/Analitics.js";
 import LastEventsFC from "./last_events/LastEvents.js";
+import OpenBankFC from './bank_account/OpenBank.js'
 
 export default function MainFC() {
 	return (
-		<main className="pl-6">
-			<section className=" rounded-2xl mb-6">
+		<main className="pl-6 openk_bank">
+			{ /* Main page */}
+			{/* <section className=" rounded-2xl mb-6">
 				<StatisticsFC />
 			</section>
-			<section className="analytics mb-4 "><AnalyticsFC /></section>
+			<section className="analytics mb-4 ">
+				<AnalyticsFC />
+			</section>
 			<section className="events rounded-2xl bg-white rounded-2xl min-h-[476px] grow pt-6">
 
 				<LastEventsFC />
+			</section> */}
+
+
+			{ /* Open банк oсcount / Открыть счет  */}
+			<section className=" rounded-2xl mb-6">
+				<OpenBankFC />
 			</section>
+
 		</main>
 	)
 }
