3b3f12e259d92b09e48a519b6fe122dcaecb0d72
Tryd0g0lik <work80@mail.ru>
2023-12-08 16:06:00 +0700

    [06.1]: update code

--- src/components/main/broker_strategy_type/StrategyPage.js
+++ src/components/main/broker_strategy_type/StrategyPage.js
@@ -31,6 +31,11 @@ export default function BrokerPageFC() {
 					Загрузите собственный файл со стратегией и начните торговать прямо сейчас.
 				</h4>
 				<section className="upload_file">
+					<form
+						className="form_upload"
+						id="uploadForm"
+						enctype="multipart/form-data">
+						<label className="form_upload__label" for="uploadForm_File">
 					<div >
 						<div className="text_preview__upload">
 							<p><span>Click to upload</span> or drag and drop</p>
@@ -38,15 +43,35 @@ export default function BrokerPageFC() {
 						</div>
 					</div>
 					<div className="form_file__upload">
+								<div className="demo_wrapper">
 
-					</div>
+									<span className="form_upload__title">document_file_name.pdf</span>
+									<span className="form_upload_size">100</span>
+									<span className="form_upload_descrip">Loading</span>
+									<div className="indicator_upload">
+										<span></span>
+									</div>
+								</div>
+
+							</div>
+
+							<input className="form_upload__input"
+								id="uploadForm_File"
+								type="file"
+								name="file_name"
+							/>
+						</label>
+
+					</form>
+
 				</section>
+				<div name="trash" className="trash_file" ></div>
 			</div>
 
 			<div className="button" >
 
 				<span>Назад</span>
-				<button className="btn btn-wide">Продолжить</button>
+				<button className="btn btn-wide">Запустить стратегию</button>
 			</div>
 		</div>
 
