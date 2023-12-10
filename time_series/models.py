48538cc630468dc4b427f9eb6c4c38e484ed3f07
VLZaigraev <v.zaigraev1@alumni.nsu.ru>
2023-12-10 12:10:16 +0300

    add usage notebook

--- time_series/models.py
+++ time_series/models.py
@@ -5,7 +5,7 @@ from typing import List
 
 warnings.filterwarnings(action="ignore")
 
-from etna.ensembles import StackingEnsemble
+# from etna.ensembles import StackingEnsemble
 
 from etna.datasets import TSDataset
 
@@ -14,13 +14,16 @@ from etna.analysis import (
     get_anomalies_density,
     get_anomalies_prediction_interval,
     get_anomalies_hist,
+    plot_anomalies
 )
 
 
 from etna.models import CatBoostMultiSegmentModel
 from etna.analysis import ModelRelevanceTable, RelevanceTable, StatisticsRelevanceTable
 from etna.analysis import get_model_relevance_table, get_statistics_relevance_table
-from etna.analysis import plot_backtest
+from etna.analysis import plot_backtest, plot_forecast
+from etna.analysis import plot_feature_relevance
+from etna.analysis import ModelRelevanceTable
 
 from etna.core import load
 model = CatBoostMultiSegmentModel(thread_count=8)
@@ -39,7 +42,7 @@ from etna.transforms import TreeFeatureSelectionTransform, FilterFeaturesTransfo
 model_feature_selection = DecisionTreeRegressor(random_state=42)
 feature_selector_transform = TreeFeatureSelectionTransform(model=model_feature_selection, top_k=5)
 
-from etna.metrics import SMAPE, MAE
+from etna.metrics import SMAPE, MAE, SMAPE
 
 
 
@@ -47,16 +50,35 @@ from etna.metrics import SMAPE, MAE
 
 
 def split_data(ts, test_size):
+    """
+    Производит  train test split датасета
+    """
     return ts.train_test_split(test_size=test_size)
 
-def get_feature_importance():
-    pass
+def interpret_model(
+    ts,
+    relevance_table=ModelRelevanceTable(),
+    top_k=5,
+    relevance_params={"model": model_feature_selection},
+):
+    """
+    Визуализирует значимость топ-k признаков для предсказаний на основе ts
+    """
+    plot_feature_relevance(
+    ts,
+    relevance_table=relevance_table,
+    top_k=top_k,
+    relevance_params=relevance_params,
+)  
 
 catboost_pipe = {'model': CatBoostMultiSegmentModel(thread_count=8),
                  'transforms': []}
 from etna.pipeline import Pipeline
 
 class Trainer:
+    """
+    Интерфейс для обучения модели с задаными моделью, преобразованиями и горизонтом предсказаний
+    """
     def __init__(self,
                  target_col = 'close',
                  features_to_remove = ['1'],
@@ -69,9 +91,11 @@ class Trainer:
         self.pipe = Pipeline(**pipe_kwargs)
         self.target_col = target_col
         self.features_to_remove = features_to_remove
-        # print(self.pipe)
 
-    def convert_df_to_ts(self, df, freq='10 min'):
+    def convert_df_to_ts(self, df: pd.DataFrame, freq:str = '10 min'):
+        """
+        Переводит pd.DataFrame во временной ряд
+        """
         df = TSDataset.to_dataset(df=df)
         ts = TSDataset(df, freq=freq)
         return ts
@@ -83,7 +107,10 @@ class Trainer:
                    timestamp_column_name='begin',
                    segment_period='Y',
                    merge_timestamps = True):
-        
+        """
+        Выполняет предобработку исходного датафрейма,
+        устанавливая целевую переменную и удаляя columns_to_drop
+        """
         # df['']
         
         df.drop(columns=columns_to_drop, inplace=True)
@@ -101,25 +128,42 @@ class Trainer:
 
         return self.convert_df_to_ts(df)
     def fit(self, train_ts):
+        """
+        Обучает модель из self.pipe на обучающем множестве
+        """
         self.pipe = self.pipe.fit(ts=train_ts, save_ts=False)
 
     def forecast(self, ts):
+        """
+        Возвращает прогноз на заданном временном ряде
+        """
         preds = self.pipe.forecast(ts=ts)
 
         return preds
     
 
     def do_backtest(self, ts, metrics, **backtest_kwargs):
+        """
+        Производит бэктест на заданном временном ряде
+        Параметры backtest конфигурируются через изменение backtest_kwargs
+        """
         metrics_df, forecast_df, fold_info_df = self.pipe.backtest(ts=ts, metrics=metrics, **backtest_kwargs)
         return metrics_df, forecast_df, fold_info_df
 
-    def plot_backtest(self, ts, forecast_df, history_len, segments):
-        plot_backtest(forecast_df, ts, history_len=100, segments=ts.segments[:4])
+    def plot_backtest(self, ts, forecast_df, history_len, n_segments):
+        
+        plot_backtest(forecast_df, ts, history_len=history_len, segments=ts.segments[:n_segments])
 
     def save_pipeline(self, path = 'pipeline.zip'):
+        """
+        Сохранение пайплайна на диске
+        """
         self.pipe.save(path)
 
     def load_pipeline(self, path):
+        """
+        Загрузка  пайплайна с диска
+        """
         return load(path)
 
 
@@ -140,4 +184,3 @@ if __name__ == "__main__":
 
     backtest_res = tr.do_backtest(test_ts, metrics=[SMAPE(), MAE()])
     print(backtest_res[0])
-    # print(ts.df.index)
--- time_series/utils.py
+++ time_series/utils.py
@@ -8,7 +8,6 @@ default_starttime = '2023-10-10'
 now_datetime = datetime.datetime.now().strftime("%Y-%m-%d")
 
 VALID_PERIODS = ['1m', '10m', '1h', 'D', 'W', 'M', 'Q']
-# TIMES_MAP = [1, 10, 60, 60*60, 60*60*24,]
 TIMES_MAP = {}
 TIMES_MAP['1m'] = 1
 TIMES_MAP['10m'] = TIMES_MAP['1m'] * 10
@@ -19,18 +18,15 @@ TIMES_MAP['M'] = TIMES_MAP['W'] * 4
 TIMES_MAP['Q'] = TIMES_MAP['M'] * 4
 
 
-def load_df(ticker_name: str = 'SBER',
-            datetime_range: Tuple[str] = (default_starttime, now_datetime),
-            period: str = '10m'):
-    sber = Ticker(ticker_name)
-    date, till_date = datetime_range
-    candles = sber.candles(date=date, till_date=till_date, period=period)
-    candles = pd.DataFrame(candles)
-    return candles
-
 ### For all companies
 class StatsBase:
-      
+      """
+      Базовый класс для работы со статистикой рынка
+      market_name, board_id аналогичны market_name, board_id из moexalgo
+
+      dataframes: словарь датафреймов с ключами, являющимися датами вида yyyy-mm-dd
+      secs: множество всех доступных компаний на рынке 
+      """
       def __init__(self, market_name = 'stocks', board_id = None) -> None:
             self.market_name, self.board_id = market_name, board_id
             self.market = Market(market_name, board_id)
@@ -45,14 +41,19 @@ class StatsBase:
       
 
       def add_new_date(self, name: str, date: str, csv_filepath: str | Path):
-            
+            """
+            Добавляет к статистике tradestats/orderstats данные за еще один день
+            Чтение данных ведется через API Algopack / загруженный csv
+            """
             new_stats, new_secs = self.get_stats(name,date, csv_filepath)
             self.dataframes[date] = new_stats
             self.secs.update(new_secs)
 
 
       def get_stats(self, name: str, date: str, csv_filepath: str | Path):
-            
+            """
+            Возвращает конкретный вид статистики по рынку (tradestats/orderstats)
+            """
             if csv_filepath is None:
                   df = getattr(self.market, name)(date=date)
                   df = pd.DataFrame(df)
@@ -64,14 +65,17 @@ class StatsBase:
             return df, set(unique_secs)
 
 class Tradestats(StatsBase):
-      
+      """
+      Tradestats для всех компаний на рынке
+      Метод добавления новой даты использует тот же интерфейс, что и у базового класса
+      """
       def __init__(self, 
                    market_name,
                    board_id,
                    date,
                    csv_filepath=None,
                    **kwargs) -> None:
-            super().__init__(market_name, board_id)
+            super().__init__(market_name, board_id, **kwargs)
 
             self.name = 'tradestats'
             self.dataframes[date], self.secs = super().get_stats(name=self.name,
@@ -84,26 +88,33 @@ class Tradestats(StatsBase):
 
 
 class OrderStats(StatsBase):
-      
+      """
+      OrderStats для всех компаний на рынке
+      Метод добавления новой даты использует тот же интерфейс, что и у базового класса
+      """
       def __init__(self, 
                    market_name: str,
                    board_id: str,
                    date: str,
                    csv_filepath=None,
                    **kwargs) -> None:
-            super().__init__(market_name, board_id)
+            super().__init__(market_name, board_id, **kwargs)
             self.name = 'orderstats'
             self.dataframes[date], self.secs = super().get_stats(name=self.name,
                                                 date=date,
                                                 csv_filepath=csv_filepath)
             
       def add_new_date(self, new_date, csv_filepath=None):
+            
             super().add_new_date(name=self.name, date=new_date, csv_filepath=csv_filepath)
             
 ### For single company
 
 class CompanyStats:
-
+      """
+      Класс для работы со статистикой коннкретной компании
+      name, period аналогичны name, period из Algopack API
+      """
       def __init__(self, name = "SBER", default_period = '10m') -> None:
             self.name = name
             self.default_period = default_period
