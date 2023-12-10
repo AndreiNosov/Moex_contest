08c8c4d3f44331499ab36d1bb0d69853b5d8c3a7
Khatskelevich Aleksey Yuryevich <akhatskelevich@ozon.ru>
2023-12-10 09:41:59 +0300

    feat: added routing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.js';
 
+import {
+	createBrowserRouter,
+	RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+]);
+
+
 // import './styles/style.css'
 import './app.css';
-// 
+//
 createRoot(document.getElementById('root')).render(
-	// <StrictMode>
-	<App />
-	//* </StrictMode> 
+	<RouterProvider router={router} />
 );
